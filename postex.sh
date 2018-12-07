5dd6b02a7473870b0a94a7c22f5d9bbb29baca7f
Phreaklets <you@example.com>
2018-12-07 13:34:58 +0000

    Added MacOS-specific features

--- postex.sh
+++ postex.sh
@@ -67,16 +67,26 @@ echo "--------------------------------------------------------------------------
 echo "[+] total memory $(cat /proc/meminfo | grep MemTotal)"
 echo "------------------------------------------------------------------------------------------"
 echo "[+] nearby hosts from the ARP table"
-arp -n | cut -d " " -f 1 | grep -v Address
+arp -n -a
 echo "------------------------------------------------------------------------------------------"
 echo "[+] DNS servers from /etc/resolv.conf"
 grep nameserver /etc/resolv.conf | grep nameserver | cut -d " " -f 2
 echo "------------------------------------------------------------------------------------------"
-echo "[+] local IP address $(ip route get 1 | awk '{print $NF;exit}')"
+if [ -e "/usr/sbin/ip" ] || [ -e "/sbin/ip" ]
+then     
+    echo "[+] local IP address $(ip route get 1 | awk '{print $NF;exit}')"
+else
+    echo "[+] local IP address $(ifconfig | grep -v inet6 | grep inet| grep -v '127.0.0.1' |  cut -d ' ' -f 2)"
+fi
 echo "------------------------------------------------------------------------------------------"
 echo "[+] external IP address $(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | sed 's/^.\(.*\).$/\1/')"
 echo "------------------------------------------------------------------------------------------"
-echo "[+] default route $(ip ro show | grep "default via" | cut -d " " -f 3)"
+if [ -e "/usr/sbin/ip" ] || [ -e "/sbin/ip" ]
+then     
+    echo "[+] default route $(ip ro show | grep "default via" | cut -d " " -f 3)"
+else
+    echo "[+] default route $(netstat -nr | grep default | cut -d ' ' -f 13)"
+fi
 echo "------------------------------------------------------------------------------------------"
 echo "[+] local listening ports (non-root)"
 netstat -anop 2>/dev/null | grep LISTEN | cut -d " " -f 15 | grep ::: | cut -c 4-
@@ -107,5 +117,43 @@ then
 else
     echo "[-] No SSH DSA key present in $HOME/.ssh/id_dsa"
 fi
+if [ -e $HOME/.ssh/id_ecdsa ]
+then
+    echo "[+] SSH ECDSA key present in $HOME/.ssh/id_dsa"
+else
+    echo "[-] No SSH ECDSA key present in $HOME/.ssh/id_dsa"
+fi
+# check for users query tool
+echo "------------------------------------------------------------------------------------------"
+if [ -e /usr/bin/users ]
+then
+    echo "[+] User accounts"
+    users
+else
+    echo "[-] dscl not present - this machine is likely not a MacOS machine"
+fi
+
+# check for dscl MacOS query tool
+echo "------------------------------------------------------------------------------------------"
+if [ -e /usr/sbin/scutil ]
+then
+    echo "[+] MacOS Active Directory domain name"
+    echo show com.apple.opendirectoryd.ActiveDirectory | scutil | grep DomainNameFlat | awk '{print $3}'
+else
+    echo "[-] scutil not present - this machine is likely not a MacOS machine"
+fi
+
+# check for dscl MacOS query tool
+echo "------------------------------------------------------------------------------------------"
+if [ -e /usr/sbin/dsconfigad ]
+then
+    echo "[+] MacOS Active Directory forest and domain name"
+    dsconfigad -show | grep "Active Directory Forest" | cut -d '=' -f 2
+    dsconfigad -show | grep "Active Directory Domain" | cut -d '=' -f 2
+else
+    echo "[-] scutil not present - this machine is likely not a MacOS machine"
+fi
+
+
 
 # TODO clear bash history
